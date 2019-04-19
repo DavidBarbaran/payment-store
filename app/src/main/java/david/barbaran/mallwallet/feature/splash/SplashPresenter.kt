37ede51b075ea11b2fca425fe535b1b7c0e15db6
DavidBarbaran <alejandro.david.barbaran@gmail.com>
2019-04-19 17:20:05 -0500

    Add check version service

--- app/src/main/java/david/barbaran/mallwallet/feature/splash/SplashPresenter.kt
+++ app/src/main/java/david/barbaran/mallwallet/feature/splash/SplashPresenter.kt
@@ -1,10 +1,45 @@
 package david.barbaran.mallwallet.feature.splash
 
 import android.graphics.Bitmap
+import david.barbaran.domain.interactor.CheckVersionUseCase
+import david.barbaran.mallwallet.BuildConfig
 import david.barbaran.mallwallet.app.MainApplication
+import kotlinx.coroutines.*
 import java.io.ByteArrayOutputStream
 
-class SplashPresenter(private val controller: SplashController.View) : SplashController.Presenter {
+class SplashPresenter(
+    private val controller: SplashController.View,
+    private val checkVersionUseCase: CheckVersionUseCase
+) : SplashController.Presenter {
+
+    private val myScope = CoroutineScope(Dispatchers.Main)
+
+    override fun checkVersion() {
+        /*myJob = CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.Main) {
+                checkVersionUseCase.checkVersion().await().let {
+                    Log.e("checkVersion","${it.lastVersionSupported}")
+                }
+            }
+        }
+        */
+
+        myScope.launch  {
+            val checkVersion = withContext(Dispatchers.IO) {
+                checkVersionUseCase.checkVersion().await()
+            }
+            if (checkVersion.lastVersionSupported > BuildConfig.VERSION_CODE){
+
+            }
+        }
+
+        /*
+        GlobalScope.launch{
+            checkVersionUseCase.checkVersion().await().let {
+                Log.e("checkVersion","${it.lastVersionSupported}")
+            }
+        }*/
+    }
 
     override fun processImage(bitmap: Bitmap) {
         val stream = ByteArrayOutputStream()
